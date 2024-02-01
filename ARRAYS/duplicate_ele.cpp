8401d5d2f5fb1502c049624a13ec77c133b3f821
pragya1204 <135381931+pragya1204@users.noreply.github.com>
2024-02-02 00:45:54 +0530

    Create duplicate_ele.cpp

---
+++ ARRAYS/duplicate_ele.cpp
@@ -0,0 +1,15 @@
+//https://www.codingninjas.com/studio/problems/duplicate-in-array_893397
+#include<vector>
+#include<iostream>
+int findDuplicate(vector<int> &arr) 
+{
+    int ans=0;
+    for(int i=0;i<arr.size();i++){
+        ans= ans^arr[i];
+    }
+    for(int i=1;i<arr.size();i++){
+        ans=ans^i;
+    }
+    return ans;
+	
+}
