4b3ddd42ecb01fed4b61c40dcbf4582b2603cda3
pragya1204 <135381931+pragya1204@users.noreply.github.com>
2024-02-06 17:31:21 +0530

    arrays

---
+++ ARRAYS/tempCodeRunnerFile.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void reverseArray(vector<int> &arr , int m){
+    vector<int> :: iterator iter1=arr.begin()+m+1;
+    vector<int>:: iterator iter=arr.end()-1;
+    
+    
+    while(iter1<=iter){
+        swap(*iter,*iter1);
+        iter1++;
+        iter--;
+        
+    }
+
+}
+int main(){
+    vector<int> v;
+    v.push_back(2);
+    v.push_back(4);
+    v.push_back(6);
+    v.push_back(8);
+    v.push_back(10);
+    v.push_back(12);
+    v.push_back(14);
+
+    reverseArray(v,3);
+    for (vector<int>::iterator i=v.begin();i<v.end();i++){
+        cout<<*i<<endl;
+    }
+    return 0;
+}
